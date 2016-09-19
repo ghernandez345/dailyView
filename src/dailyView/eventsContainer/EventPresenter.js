95ee0822fb867918c0990297afd24f592e26bd4b
Gabe Hernandez <gabriel.hernandez@lab49.com>
2016-09-19 20:58:05 +0100

    docs

--- src/dailyView/eventsContainer/EventPresenter.js
+++ src/dailyView/eventsContainer/EventPresenter.js
@@ -1,6 +1,10 @@
 /**
- * Responsible for generating the collection of events
- * to render into the UI.
+ * Responsible for generating the collection of events to render into the UI.
+ * Consider the events in the UI to be group into columns. An event will try to
+ * be in the furthest left column it can be in without a collision. When
+ * we do this, we end with a 2D martix of event data, where each event in a
+ * group will have the same width and left offset, which is determined by the
+ * total number of groups.
  */
 
 /**
@@ -35,18 +39,20 @@ function packEvents(eventColumns, viewWidth) {
     const eventColumn = eventColumns[i];
 
     for (let j = 0; j < eventColumn.length; j++) {
-      var eventTag = eventColumn[j];
-      eventTag.left = calculateLeftOffset(i, numEventColumns);
-      eventTag.width = calculateWidth(viewWidth, numEventColumns);
+      var eventBubble = eventColumn[j];
+      eventBubble.left = calculateLeftOffset(i, numEventColumns);
+      eventBubble.width = calculateWidth(viewWidth, numEventColumns);
     }
   }
 }
+
 
 export const EventPresenter = {
 
   generateEventPresenterData(events, viewWidth) {
     let prevEventEnd = null;
     let eventColumns = [];
+
     events = sortEvents(events);
 
     events.forEach((event, i) => {
@@ -65,16 +71,15 @@ export const EventPresenter = {
       // Try to place the event inside the existing columns
       let isInserted = false;
       for (let i = 0; i < eventColumns.length; i++) {
-        let col = eventColumns[i];
-        if (!collidesWith(col[col.length-1], event)) {
-          col.push(event);
+        let eventColumn = eventColumns[i];
+        if (!collidesWith(eventColumn[eventColumn.length-1], event)) {
+          eventColumn.push(event);
           isInserted = true;
           break;
         }
       }
 
-      // It was not possible to place the event. Add a new column
-      // for the current event group.
+      // It was not possible to place the event. Add a new event group.
       if (!isInserted) {
         eventColumns.push([event]);
       }
